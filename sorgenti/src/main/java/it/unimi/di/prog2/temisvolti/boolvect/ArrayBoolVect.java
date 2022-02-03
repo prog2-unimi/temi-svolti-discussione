0c6545b341ca339273f08e7fd42eb5c69181f26b
Massimo Santini <santini@di.unimi.it>
2022-02-03 11:44:33 +0100

    First release of BoolVect

---
+++ sorgenti/src/main/java/it/unimi/di/prog2/temisvolti/boolvect/ArrayBoolVect.java
@@ -0,0 +1,95 @@
+/*
+
+Copyright 2021 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.temisvolti.boolvect;
+
+import java.util.Arrays;
+
+/** Classe concreta che implementa un {@link BoolVect} denso di taglia assegnata. */
+public class ArrayBoolVect extends AbstractBoolVect {
+
+  /** La dimensione del BoolVect. */
+  private int dimensione = 0;
+
+  /** I valori del BoolVect. */
+  private final boolean[] valore;
+
+  // RI: * 0 <= dimensione <= valore.length
+  //     * valore[dimensione - 1] == true && valore[p] == false se p >= dimensione
+  //     * valore != null
+  // AF: BoolVect di taglia valore.length il cui i--esimo valore di verità è l'i-esimo elemento di
+  // valore
+
+  /**
+   * Costruisce un BoolVect di taglia assegnata.
+   *
+   * @param taglia la taglia
+   * @throws IllegalArgumentException se la taglia è <= 0.
+   */
+  public ArrayBoolVect(final int taglia) {
+    if (taglia <= 0) throw new IllegalArgumentException("La taglia deve essere positiva.");
+    valore = new boolean[taglia];
+  }
+
+  /** Costruisce un BoolVect di taglia 1024. */
+  public ArrayBoolVect() {
+    this(1024);
+  }
+
+  @Override
+  public int dimensione() {
+    return dimensione;
+  }
+
+  @Override
+  public int taglia() {
+    return valore.length;
+  }
+
+  @Override
+  public void pulisci() {
+    Arrays.fill(valore, false);
+  }
+
+  @Override
+  public boolean leggiParziale(final int pos) {
+    return valore[pos];
+  }
+
+  @Override
+  public void scriviParziale(final int pos, final boolean val) {
+    valore[pos] = val;
+    if (val && pos >= dimensione) dimensione = pos + 1;
+    if (!val && pos == dimensione - 1)
+      while (dimensione > 0 && !valore[dimensione - 1]) dimensione--;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof ArrayBoolVect) return Arrays.equals(valore, ((ArrayBoolVect) obj).valore);
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(valore);
+  }
+}
---
+++ sorgenti/src/test/java/it/unimi/di/prog2/temisvolti/boolvect/BoolVectTest.java
@@ -0,0 +1,160 @@
+/*
+
+Copyright 2021 Massimo Santini
+
+This file is part of "Programmazione 2 @ UniMI" teaching material.
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This material is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+package it.unimi.di.prog2.temisvolti.boolvect;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class BoolVectTest {
+
+  static List<Class<? extends BoolVect>> IMPLEMENTATIONS =
+      List.of(ArrayBoolVect.class, LongBoolVect.class, SetBoolVect.class);
+
+  record ImplPair(Class<? extends BoolVect> u, Class<? extends BoolVect> v) {
+    public BoolVect newInstanceU() {
+      try {
+        return (BoolVect) u.getConstructor().newInstance();
+      } catch (Exception e) {
+        return null;
+      }
+    }
+
+    public BoolVect newInstanceV() {
+      try {
+        return (BoolVect) v.getConstructor().newInstance();
+      } catch (Exception e) {
+        return null;
+      }
+    }
+  }
+
+  static Stream<ImplPair> ulist() {
+    List<ImplPair> wrap = new ArrayList<>();
+    for (Class<? extends BoolVect> u : IMPLEMENTATIONS) wrap.add(new ImplPair(u, null));
+    return wrap.stream();
+  }
+
+  static Stream<ImplPair> uvpairs() {
+    List<ImplPair> crossprod = new ArrayList<>();
+    for (Class<? extends BoolVect> u : IMPLEMENTATIONS)
+      for (Class<? extends BoolVect> v : IMPLEMENTATIONS) crossprod.add(new ImplPair(u, v));
+    return crossprod.stream();
+  }
+
+  @ParameterizedTest
+  @MethodSource("ulist")
+  void testDaStringaV(ImplPair one) {
+    BoolVect u = one.newInstanceU();
+    u.daString("VVF");
+    final String us = "VFFFVVVFFFVV";
+    u.daString(us);
+    assertEquals(u.toString(), us);
+  }
+
+  @ParameterizedTest
+  @MethodSource("ulist")
+  void testDaStringaF(ImplPair one) {
+    BoolVect u = one.newInstanceU();
+    u.daString("VVF");
+    final String us = "FFFFVFFFVVVFFFVV", expected = "VFFFVVVFFFVV";
+    u.daString(us);
+    assertEquals(u.toString(), expected);
+  }
+
+  @ParameterizedTest
+  @MethodSource("ulist")
+  void testToString0(ImplPair one) {
+    BoolVect u = one.newInstanceU();
+    assertEquals(u.toString(), "F");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testAnd(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFF");
+    v.daString("FVFV");
+    u.and(v);
+    assertEquals(u.toString(), "VFF");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testAndDifferingDimension(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFVVVFF");
+    v.daString("VFVFV");
+    u.and(v);
+    assertEquals(u.toString(), "VFVFF");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testOr(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFF");
+    v.daString("FVFV");
+    u.or(v);
+    assertEquals(u.toString(), "VVFV");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testOrDifferingDimension(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFVVVFF");
+    v.daString("VFVFV");
+    u.or(v);
+    assertEquals(u.toString(), "VVFVVVFV");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testXor(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFF");
+    v.daString("FVFV");
+    u.xor(v);
+    assertEquals(u.toString(), "VFFV");
+  }
+
+  @ParameterizedTest
+  @MethodSource("uvpairs")
+  void testXorDifferingDimension(ImplPair pair) {
+    BoolVect u = pair.newInstanceU();
+    BoolVect v = pair.newInstanceV();
+    u.daString("VVFVVVFF");
+    v.daString("VFVFV");
+    u.xor(v);
+    assertEquals(u.toString(), "VVFFVFFV");
+  }
+}
